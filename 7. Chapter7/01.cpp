71771ccb811e13a414e68eeb5e5ccd937fa5905e
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-17 21:23:28 +0530

    Add files via upload

---
+++ 7. Chapter7/01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            cout << "*"<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ 7. Chapter7/markdown.md
@@ -0,0 +1,8 @@
+# Pattern Programs 
+
+
+
+`` Soon we will update the Best Problems. ``
+
+
+> Hello Guys This is Just a pattern programs practice Set.

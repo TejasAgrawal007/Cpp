5846d57fb1f3150dce0e84a44d1b7d189ee8378e
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-28 14:57:32 +0530

    Add files via upload

---
+++ Project 1/color.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <windows.h> // Sleep (milisecounds);
+using namespace std;
+int main()
+{
+    cout << "\n\n\t\tText Color";
+
+    system("color 01");
+    Sleep(2000);
+
+    system("color 02");
+    Sleep(2000);
+
+    system("color 03");
+    Sleep(2000);
+
+    system("color 04");
+    Sleep(2000);
+
+    system("color 05");
+    Sleep(2000);
+
+    system("color 06");
+    Sleep(2000);
+
+    system("color 07");
+    Sleep(2000);
+
+    system("color 08");
+    Sleep(2000);
+
+    system("color 09");
+    Sleep(2000);
+
+    system("color 0A");
+    Sleep(2000);
+
+    system("color 0B");
+    Sleep(2000);
+
+    system("color 0C");
+    Sleep(2000);
+
+    system("color 0D");
+    Sleep(2000);
+
+    system("color 0E");
+    Sleep(2000);
+
+    system("color 0F");
+    Sleep(2000);
+
+    system("cls");
+
+    system("color 01");
+
+    cout << "\n\n\t\tBackground Color";
+    system("color 10");
+    Sleep(2000);
+
+    system("color 20");
+    Sleep(2000);
+
+    system("color 30");
+    Sleep(2000);
+
+    system("color 40");
+    Sleep(2000);
+
+    system("color 50");
+    Sleep(2000);
+
+    system("color 60");
+    Sleep(2000);
+
+    system("color 70");
+    Sleep(2000);
+
+    system("color 80");
+    Sleep(2000);
+
+    system("color 90");
+    Sleep(2000);
+
+    system("pause");
+
+    return 0;
+}
